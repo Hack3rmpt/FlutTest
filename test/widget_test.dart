6b3c9e73c71ac45a6a644140d3f398912b76193c
Hack3rmpt <isip_d.s.kuznecov@mpt.ru>
2024-10-24 18:23:13 +0300

    asdasd

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -27,5 +27,5 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
-  //not god world!!!
+  //not god world!!! im not a live, im deadinside
 }
